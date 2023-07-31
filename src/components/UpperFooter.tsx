4ad58a972bd9f96d980c39fa91a3658cfaa8c5c6
Linus Hernvall <linus.hernvall@gmail.com>
2023-07-31 15:06:45 +0200

    UpperFooter added

--- src/components/UpperFooter.tsx
+++ src/components/UpperFooter.tsx
@@ -4,16 +4,27 @@ function UpperFooter() {
   return (
     <div>
       <Box sx={topShadowSx} />
-      <Box sx={{ height: "150px" }}>UPPERFOTTER</Box>
+      <Box sx={upperFooterWrapperSx}>
+        <img
+          src="../../images/lh-logotype.png"
+          alt="LH Custom Picks logotype"
+        />
+      </Box>
     </div>
   );
 }
 
 // CSS ----------------------------------------------
 
+const upperFooterWrapperSx: SxProps = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "10px 0",
+};
+
 const topShadowSx: SxProps = {
   height: "1px",
-  //   boxShadow: "0px 3px 1px 0px, rgba(0, 0, 0, 0.23)",
   boxShadow: "0px 3px 3px rgba(0,0,0, 0.23)",
 };
 
